03389f3f15908f790897c322563628bda60087df
unknown <ebubeidika@gmail.com>
2024-01-27 09:46:50 +0100

    Second commit

---
+++ videotransmission.py
@@ -0,0 +1,29 @@
+from vidstream import CameraClient
+from vidstream import StreamingServer
+
+import threading
+import time
+
+
+
+receiving = StreamingServer('192.168.80.138',9999)
+
+sending =CameraClient('10.138.39.121',9999)
+
+
+t1=threading.Thread(target=receiving.start_server)
+t1.start()
+time.sleep(5)
+
+
+t2=threading.Thread(target=sending.start_stream)
+t2.start()
+
+
+
+while input("")!="STOP":
+    continue
+
+
+receiving.stop_server()
+sending.stop_stream()
